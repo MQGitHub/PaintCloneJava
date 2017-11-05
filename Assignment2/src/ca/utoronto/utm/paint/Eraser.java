7d25232588b54158aeae95d08303c5d9cfa3e3ec
hassanhjk5 <hassan@magiclobe.com>
2017-11-05 18:16:24 -0500

    Finished Javadoc for Eraser, Line, PaintModel, and Squiggle.

--- Assignment2/src/ca/utoronto/utm/paint/Eraser.java
+++ Assignment2/src/ca/utoronto/utm/paint/Eraser.java
@@ -3,15 +3,46 @@ package ca.utoronto.utm.paint;
 import java.awt.Color;
 import java.util.ArrayList;
 
-public class Eraser extends Squiggle{
+/**
+ * Represents the eraser for our paint application. Containing
+ * the points where the eraser was used, the erasers thickness
+ * , and the color of the eraser.  
+ *
+ */
+public class Eraser extends Squiggle {
+	
+	
+	/**
+	 *  Constructor adds the points where the eraser is used
+	 *  and color. Sets the thickness to 20.
+	 *  
+	 *  @param c 	   The color of the eraser.
+	 *  @param points  All of the points where eraser is used. 
+	 *  @see  		   Squiggle. 
+	 */
 	public Eraser(Color c, ArrayList<Point> points) {
 		super(c, 20, points);
 	}
 	
-	public ArrayList<Point> getPoints(){
+	/**
+	 * Returns an ArrayList containing all the points where eraser
+	 * have been used.
+	 * 
+	 * @return 	ArrayList containing all the current points for eraser.
+	 * @see 	Squiggle
+	 */
+	public ArrayList<Point> getPoints() {
 		return super.getPoints();
 	}
-	
+
+	/**
+	 * Adds to the ArrayList containing all the points for all the
+	 * times eraser was used.
+	 * 
+	 * @param p	 adds given point to pts ArrayList containing all previous
+	 * 		     points. The ArrayList is held in the parent class Squiggle.
+	 * @see   Squiggle.
+	 */
 	public void addPoint(Point p) {
 		super.addPoint(p);
 	}
--- Assignment2/src/ca/utoronto/utm/paint/Line.java
+++ Assignment2/src/ca/utoronto/utm/paint/Line.java
@@ -2,19 +2,46 @@ package ca.utoronto.utm.paint;
 
 import java.awt.Color;
 
-public class Line extends Shape implements DrawShape{
+/**
+ * Represents the lines for our paint application. Containing all the lines
+ * color, thickness, and start and end points.
+ *
+ */
+public class Line extends Shape implements DrawShape {
 	private Point begin;
 	private Point end;
-	
+
+	/**
+	 * Creates a line with attributes color, thickness, if the line is filled, start
+	 * and end point.
+	 * 
+	 * @param c      the color of the line.
+	 * @param t      a int that indicates how thick the line should be.
+	 * @param b      a boolean which indicates if the line should be filled.
+	 * @param begin  a point that is the start point of the line.
+	 * @param end    a point that is end point of the line.
+	 * @see Shape
+	 * @see DrawShape
+	 */
 	public Line(Color c, int t, Boolean b, Point begin, Point end) {
 		super(c, t, b, begin);
 		this.end = end;
 	}
-	
+
+	/**
+	 * Sets the end point to the point given.
+	 * 
+	 * @param end a point that is end point of the line.
+	 */
 	public void setEndPoint(Point end) {
 		this.end = end;
 	}
-	
+
+	/**
+	 * Returns the end point of the line.
+	 * 
+	 * @return end the end point of the line.
+	 */
 	public Point getEndPoint() {
 		return this.end;
 	}
@@ -22,6 +49,6 @@ public class Line extends Shape implements DrawShape{
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
