3fca5ce76fbdf2002108fdbef0506b37993fa305
Radzzzzzzzz <radhika_lakhtakia@hotmail.ca>
2017-11-05 00:44:36 -0400

    Squiggle can be drawn over shapes.

--- Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
+++ Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
@@ -1,6 +1,6 @@
 package ca.utoronto.utm.paint;
 
-import javax.swing.*;  
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -12,11 +12,11 @@ import java.util.Observer;
 // https://docs.oracle.com/javase/8/docs/api/java/awt/Graphics2D.html
 // https://docs.oracle.com/javase/tutorial/2d/
 
-class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseListener  {
-	private int i=0;
+class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseListener {
+	private int i = 0;
 	private PaintModel model; // slight departure from MVC, because of the way painting works
 	private View view; // So we can talk to our parent or other components of the view
-	
+
 	private String mode; // modifies how we interpret input (could be better?)
 	private Circle circle; // the circle we are building
 	private Point begin, end;
@@ -30,107 +30,108 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 	private Color colour; // keeps track of the current color
 	private int thickness;
 	private Point squiggleBegin;
-	
-	public PaintPanel(PaintModel model, View view){
+
+	public PaintPanel(PaintModel model, View view) {
 		this.setBackground(Color.blue);
 		this.colour = Color.white;
-		this.setPreferredSize(new Dimension(300,300));
+		this.setPreferredSize(new Dimension(300, 300));
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
-		
-		this.mode="Circle"; // bad code here?
-		
+
+		this.mode = "Circle"; // bad code here?
+
 		this.model = model;
 		this.model.addObserver(this);
-		
-		this.view=view;
+
+		this.view = view;
 	}
 
 	/**
-	 *  View aspect of this
+	 * View aspect of this
 	 */
 	public void paintComponent(Graphics g) {
 		// Use g to draw on the JPanel, lookup java.awt.Graphics in
 		// the javadoc to see more of what this can do for you!!
-		
-        super.paintComponent(g); //paint background
-        Graphics2D g2d = (Graphics2D) g; // lets use the advanced api
-		// setBackground (Color.blue); 
+
+		super.paintComponent(g); // paint background
+		Graphics2D g2d = (Graphics2D) g; // lets use the advanced api
+		// setBackground (Color.blue);
 		// Origin is at the top left of the window 50 over, 75 down
 		g2d.setColor(Color.white);
-        g2d.drawString ("i="+i, 25, 25);
-		i=i+1;
+		g2d.drawString("i=" + i, 25, 25);
+		i = i + 1;
 
-		// Draw Lines
-		ArrayList<Point> points = this.model.getPoints();
-		for(int i=0;i<points.size()-1; i++){
-			Point p1=points.get(i);
-			Point p2=points.get(i+1);
-			g2d.setColor(p1.getColor());
-			g2d.setStroke(new BasicStroke(p1.getThickness()));
-			if ((p1.getX() == -1 && p1.getY() == -1) || (p2.getX() == -1 && p2.getY() == -1)) {
-				i = i + 2;
-			}else {
-				g2d.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
-			}
-		}
-		
 		ArrayList<Shape> shapes = this.model.getShapes();
-		for(Shape s: this.model.getShapes()) {
-			if(s instanceof Circle) {
+		for (Shape s : this.model.getShapes()) {
+			if (s instanceof Circle) {
 				int x = s.getCorner().getX();
 				int y = s.getCorner().getY();
 				int radius = ((Oval) s).getHeight();
 				g2d.setColor(s.getColor());
 				g2d.setStroke(new BasicStroke(s.getThickness()));
-				if(s.isFilled()) {
-					g2d.fillOval(x-radius,y-radius, radius*2, radius*2);
-				}else {
-					g2d.drawOval(x-radius,y-radius, radius*2, radius*2);
+				if (s.isFilled()) {
+					g2d.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+				} else {
+					g2d.drawOval(x - radius, y - radius, radius * 2, radius * 2);
 				}
-			}else if(s instanceof Oval){
+			} else if (s instanceof Oval) {
 				int x = s.getCorner().getX();
 				int y = s.getCorner().getY();
-				int height = ((Oval)s).getHeight();
-				int width = ((Oval)s).getWidth();
+				int height = ((Oval) s).getHeight();
+				int width = ((Oval) s).getWidth();
 				g2d.setColor(s.getColor());
 				g2d.setStroke(new BasicStroke(s.getThickness()));
-				if(s.isFilled()) {
+				if (s.isFilled()) {
 					g2d.fillOval(x, y, width, height);
-				}else {
+				} else {
 					g2d.drawOval(x, y, width, height);
 				}
-			}else if(s instanceof Square) {
+			} else if (s instanceof Square) {
 				int x = s.getCorner().getX();
 				int y = s.getCorner().getY();
-				int width = ((Square)s).getWidth();
+				int width = ((Square) s).getWidth();
 				g2d.setColor(s.getColor());
 				g2d.setStroke(new BasicStroke(s.getThickness()));
-				if(s.isFilled()) {
-					g2d.fillRect(x-width, y-width, width*2, width*2);
-				}else {
-					g2d.drawRect(x-width, y-width, width*2, width*2);
+				if (s.isFilled()) {
+					g2d.fillRect(x - width, y - width, width * 2, width * 2);
+				} else {
+					g2d.drawRect(x - width, y - width, width * 2, width * 2);
 				}
-			}else if(s instanceof Rectangle){
+			} else if (s instanceof Rectangle) {
 				int x = s.getCorner().getX();
 				int y = s.getCorner().getY();
-				int height = ((Rectangle)s).getHeight();
-				int width = ((Rectangle)s).getWidth();
+				int height = ((Rectangle) s).getHeight();
+				int width = ((Rectangle) s).getWidth();
 				g2d.setColor(s.getColor());
 				g2d.setStroke(new BasicStroke(s.getThickness()));
-				if(s.isFilled()) {
+				if (s.isFilled()) {
 					g2d.fillRect(x, y, width, height);
-				}else {
+				} else {
 					g2d.drawRect(x, y, width, height);
 				}
-			}else if(s instanceof Line) {
+			} else if (s instanceof Line) {
 				Point p2 = s.getCorner();
-				Point p1 = ((Line)s).getEndPoint();
+				Point p1 = ((Line) s).getEndPoint();
 				g2d.setColor(s.getColor());
 				g2d.setStroke(new BasicStroke(s.getThickness()));
-				g2d.drawLine(p1.getX(), p1.getY(),p2.getX(), p2.getY());
+				g2d.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
 			}
 		}
+
+		// Draw Lines
+		ArrayList<Point> points = this.model.getPoints();
+		for (int i = 0; i < points.size() - 1; i++) {
+			Point p1 = points.get(i);
+			Point p2 = points.get(i + 1);
+			g2d.setColor(p1.getColor());
+			g2d.setStroke(new BasicStroke(p1.getThickness()));
+			if ((p1.getX() == -1 && p1.getY() == -1) || (p2.getX() == -1 && p2.getY() == -1)) {
+				i = i + 2;
+			} else {
+				g2d.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+			}
+		}
+
 		g2d.dispose();
 	}
 
@@ -139,79 +140,78 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 		// Not exactly how MVC works, but similar.
 		this.repaint(); // Schedule a call to paintComponent
 	}
-	
+
 	/**
-	 *  Controller aspect of this
+	 * Controller aspect of this
 	 */
-	public void setMode(String mode){
-		this.mode=mode;
+	public void setMode(String mode) {
+		this.mode = mode;
 	}
-	
 
 	public void setFill(boolean fill) {
 		this.filled = fill;
 	}
-	
+
 	public void setThickness(int thickness) {
 		this.thickness = thickness;
 
 	}
-	
+
 	// MouseMotionListener below
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		if(this.mode=="Squiggle"){
-			
-		} else if(this.mode=="Circle"){
-			
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+
 		}
 	}
+
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		int min_X = Math.min(begin.getX(), e.getX());
 		int min_Y = Math.min(begin.getY(), e.getY());
 		int max_X = Math.max(begin.getX(), e.getX());
 		int max_Y = Math.max(begin.getY(), e.getY());
-		if(this.mode=="Squiggle"){
-			this.model.addPoint(new Point(this.colour, thickness,e.getX(), e.getY()));
-		} else if(this.mode=="Circle"){
-			int x = begin.getX()-e.getX();
-			int y = begin.getY()-e.getY();
-			int radius = (int) Math.sqrt(Math.pow(x,2) + 
-					Math.pow(y, 2));
+		if (this.mode == "Squiggle") {
+			this.model.addPoint(new Point(this.colour, thickness, e.getX(), e.getY()));
+		} else if (this.mode == "Circle") {
+			int x = begin.getX() - e.getX();
+			int y = begin.getY() - e.getY();
+			int radius = (int) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 			this.circle.setRadius(radius);
 			this.model.addShape(this.circle);
-		}else if(this.mode == "Rectangle") {
+		} else if (this.mode == "Rectangle") {
 			this.rectangle.setCorner(new Point(min_X, min_Y));
 			this.rectangle.setWidth(max_X - min_X);
 			this.rectangle.setHeight(max_Y - min_Y);
 			this.model.addShape(this.rectangle);
-		}else if(this.mode =="Square") {
+		} else if (this.mode == "Square") {
 			int x = begin.getX() - e.getX();
 			int y = begin.getY() - e.getY();
-			int width = (int) Math.sqrt(Math.pow(x,2) +  Math.pow(y, 2));
+			int width = (int) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 			this.square.setWidth(width);
 			this.model.addShape(this.square);
-		}else if(this.mode=="Line") {
+		} else if (this.mode == "Line") {
 			end = new Point(e.getX(), e.getY());
 			this.line.setEndPoint(end);
 			this.model.addShape(this.line);
-		}else if(this.mode=="Oval") {
+		} else if (this.mode == "Oval") {
 			this.oval.setCorner(new Point(min_X, min_Y));
 			this.oval.setWidth(max_X - min_X);
 			this.oval.setHeight(max_Y - min_Y);
 			this.model.addShape(this.oval);
 		}
-		
+
 		repaint();
 	}
 
 	// MouseListener below
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if(this.mode=="Squiggle"){
-			
-		} else if(this.mode=="Circle"){
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
 		}
 	}
 
@@ -219,53 +219,52 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 	public void mousePressed(MouseEvent e) {
 		begin = new Point(e.getX(), e.getY());
 
-		if(this.mode=="Squiggle"){
+		if (this.mode == "Squiggle") {
 			squiggleBegin = new Point(this.colour, thickness, e.getX(), e.getY());
-		} else if(this.mode=="Circle"){
+		} else if (this.mode == "Circle") {
 			this.circle = new Circle(this.colour, thickness, filled, begin, 0);
-		}else if(this.mode=="Rectangle") {
-			this.rectangle = new Rectangle(this.colour, thickness,filled,begin,0,0);
-		}else if(this.mode=="Square") {
-			this.square = new Square(this.colour, thickness, filled,begin,0);
-		}else if(this.mode=="Line") {
-			this.line = new Line(this.colour, thickness, false, begin,begin);
-		}else if(this.mode=="Oval") {
-			this.oval = new Oval(this.colour, thickness, filled, begin,0,0);
+		} else if (this.mode == "Rectangle") {
+			this.rectangle = new Rectangle(this.colour, thickness, filled, begin, 0, 0);
+		} else if (this.mode == "Square") {
+			this.square = new Square(this.colour, thickness, filled, begin, 0);
+		} else if (this.mode == "Line") {
+			this.line = new Line(this.colour, thickness, false, begin, begin);
+		} else if (this.mode == "Oval") {
+			this.oval = new Oval(this.colour, thickness, filled, begin, 0, 0);
 		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if(this.mode=="Squiggle"){
-			this.model.addPoint(new Point(-1,-1));
-		} else if(this.mode=="Circle"){
-			if(this.circle!=null){
+		if (this.mode == "Squiggle") {
+			this.model.addPoint(new Point(-1, -1));
+		} else if (this.mode == "Circle") {
+			if (this.circle != null) {
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		if(this.mode=="Squiggle"){
-			
-		} else if(this.mode=="Circle"){
-			
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+
 		}
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		if(this.mode=="Squiggle"){
-			
-		} else if(this.mode=="Circle"){
-			
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+
 		}
 	}
-	
+
 	public void setColour(Color colour) {
 		this.colour = colour;
-		
+
 	}
 }
-	
