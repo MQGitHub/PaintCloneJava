179415750554f11dec9b9b23fd93060088d000c4
Radzzzzzzzz <radhika_lakhtakia@hotmail.ca>
2017-11-05 19:04:20 -0500

    Eraser fixed

--- Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
+++ Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
@@ -104,8 +104,10 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 				ArrayList<Point> points = ((Eraser) s).getPoints();
 				for (int i = 0; i < points.size() - 1; i++) {
 					Point p1 = points.get(i);
+					Point p2 = points.get(i + 1);
+					g2d.setStroke(new BasicStroke(s.getThickness()));
 					g2d.setColor(background);
-					g2d.fillOval(p1.getX(), p1.getY(), 20, 20);
+					g2d.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
 				}
 
 			} else if (s instanceof Rectangle) {
